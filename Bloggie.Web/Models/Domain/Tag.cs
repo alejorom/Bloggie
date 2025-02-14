fa0ef1bb99a5fba61c8d688fa6c2ff150daa0d1e
AlejandroRR <morojela-github@yahoo.comcom>
2025-02-14 09:19:23 -0500

    Adicionar BlogPost / Tag Domain Models

---
+++ Bloggie.Web/Models/Domain/Tag.cs
@@ -0,0 +1,9 @@
+﻿namespace Bloggie.Web.Models.Domain
+{
+    public class Tag
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required Guid BlogPostId { get; set; }
+    }
+}
