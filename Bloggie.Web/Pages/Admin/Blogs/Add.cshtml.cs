84dde3be27fe8cbac3e7f913024c767bb46afe30
AlejandroRR <morojela-github@yahoo.comcom>
2025-02-19 08:19:36 -0500

    Almacenar en base de datos

--- Bloggie.Web/Pages/Admin/Blogs/Add.cshtml.cs
+++ Bloggie.Web/Pages/Admin/Blogs/Add.cshtml.cs
@@ -1,3 +1,5 @@
+using Bloggie.Web.Data;
+using Bloggie.Web.Models.Domain;
 using Bloggie.Web.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -6,11 +8,37 @@ namespace Bloggie.Web.Pages.Admin.Blogs
 {
     public class AddModel : PageModel
     {
+        private readonly BloggieDbContext bloggieDbContext;
+
         [BindProperty]
         public AddBlogPost AddBlogPostRequest { get; set; }
 
+        public AddModel(BloggieDbContext bloggieDbContext)
+        {
+            this.bloggieDbContext = bloggieDbContext;
+        }
+
         public void OnGet()
         {
+        }
+
+        public void OnPost() 
+        {
+            var blogPost = new BlogPost()
+            {
+                Heading = AddBlogPostRequest.Heading,
+                PageTitle = AddBlogPostRequest.PageTitle,
+                Content = AddBlogPostRequest.Content,
+                ShortDescription = AddBlogPostRequest.ShortDescription,
+                FeatureImageUrl = AddBlogPostRequest.FeatureImageUrl,
+                UrlHandle = AddBlogPostRequest.UrlHandle,
+                PublishedDate = AddBlogPostRequest.PublishedDate,
+                Author = AddBlogPostRequest.Author,
+                Visible = AddBlogPostRequest.Visible
+            };
+
+            bloggieDbContext.BlogPosts.Add(blogPost);
+            bloggieDbContext.SaveChanges();
         }
     }
 }
