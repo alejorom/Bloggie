e381710d668096e78320a723c21c550bff9b94f8
AlejandroRR <morojela-github@yahoo.comcom>
2025-02-19 07:28:52 -0500

    Adicionar AddBlogPost view model / Binding property

---
+++ Bloggie.Web/Models/ViewModels/AddBlogPost.cs
@@ -0,0 +1,15 @@
+﻿namespace Bloggie.Web.Models.ViewModels
+{
+    public class AddBlogPost
+    {
+        public required string Heading { get; set; }
+        public required string PageTitle { get; set; }
+        public required string Content { get; set; }
+        public required string ShortDescription { get; set; }
+        public required string FeatureImageUrl { get; set; }
+        public required string UrlHandle { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public required string Author { get; set; }
+        public bool Visible { get; set; }
+    }
+}
