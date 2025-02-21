268cd6789a2401e00441f82ea8da98206f57f9c1
AlejandroRR <morojela-github@yahoo.comcom>
2025-02-21 16:08:05 -0500

    Adicionar Edit / Delete funcionalidad

---
+++ Bloggie.Web/Pages/Admin/Blogs/Edit.cshtml.cs
@@ -0,0 +1,59 @@
+using Bloggie.Web.Data;
+using Bloggie.Web.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bloggie.Web.Pages.Admin.Blogs
+{
+    public class EditModel : PageModel
+    {
+        private readonly BloggieDbContext bloggieDbContext;
+
+        [BindProperty]
+        public BlogPost BlogPost { get; set; }
+
+        public EditModel(BloggieDbContext bloggieDbContext)
+        {
+            this.bloggieDbContext = bloggieDbContext;
+        }
+
+        public void OnGet(Guid id)
+        {
+            BlogPost = bloggieDbContext.BlogPosts.Find(id);
+        }
+
+        public IActionResult OnPostEdit()
+        {
+            var existingBlogPost = bloggieDbContext.BlogPosts.Find(BlogPost.Id);
+
+            if (existingBlogPost != null)
+            {
+                existingBlogPost.Heading = BlogPost.Heading;
+                existingBlogPost.PageTitle = BlogPost.PageTitle;
+                existingBlogPost.Content = BlogPost.Content;
+                existingBlogPost.ShortDescription = BlogPost.ShortDescription;
+                existingBlogPost.FeatureImageUrl = BlogPost.FeatureImageUrl;
+                existingBlogPost.UrlHandle = BlogPost.UrlHandle;
+                existingBlogPost.PublishedDate = BlogPost.PublishedDate;
+                existingBlogPost.Author = BlogPost.Author;
+                existingBlogPost.Visible = BlogPost.Visible;
+
+                bloggieDbContext.SaveChanges();
+            }
+
+            return RedirectToPage("/Admin/Blogs/List");
+        }
+
+        public IActionResult OnPostDelete()
+        {
+            var existingBlogPost = bloggieDbContext.BlogPosts.Find(BlogPost.Id);
+            if (existingBlogPost != null)
+            {
+                bloggieDbContext.BlogPosts.Remove(existingBlogPost);
+                bloggieDbContext.SaveChanges();
+            }
+
+            return RedirectToPage("/Admin/Blogs/List");
+        }
+    }
+}
